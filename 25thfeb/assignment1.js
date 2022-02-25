e4b1b69dc9ebbc979c3e12e76b013d38c51240ca
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-02-26 01:07:37 +0530

    Create assignment1.js

---
+++ 25thfeb/assignment1.js
@@ -0,0 +1,33 @@
+console.log("Hello")
+
+//to find whether the no is greater than 10 or not
+
+var a=11;
+if(a>10)
+{
+console.log("no is greater than 10");
+}
+elseif(a=10)
+{
+console.log("no is equals to 10");
+}
+else
+{
+console.log("no is not equals to 10");
+}
+
+//greatest of 3 nos
+
+var a=1,b=2,c=3;
+if(a>b&&a>c)
+{
+  console.log("a is greatest");
+}
+elseif(b>c&&b>a)
+{
+  console.log("b is greatest");
+}
+else
+{
+  console.log("c is greatest");
+}

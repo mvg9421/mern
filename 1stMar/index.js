8790416118adc7f01825e8da7445708de67d1ab0
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-03-01 12:42:56 +0530

    Create index.js

---
+++ 1stMar/index.js
@@ -0,0 +1,101 @@
+//Hello everyone
+
+/*
+comment 1
+
+comment 2
+comment 3
+comment 4
+
+*/
+
+// var a = false
+// console.log(a);
+
+// var a = "false"
+// console.log(typeof(a));
+
+
+// var a=20;
+
+// a="bsjc";
+
+// check if the number is odd or event
+
+// var a= 16;
+
+// if(a%2==0){
+//     console.log("Number is even")
+// }else{
+//     console.log("Number is Odd")
+// }
+
+
+// console.log(10=="10");
+
+// console.log(10<10);
+
+
+// alert("Statement 1");
+
+// alert("Statement 2");
+
+
+// var a=  prompt("Enter a number");
+
+// alert(a);
+
+
+// var A=prompt("Enter any number");
+// if(A%2==0){
+//     alert("Number is even")
+// }else{
+//     alert("Number is Odd")
+// }
+
+// console.log(A);
+
+
+// correct
+// abcd  
+// _abcd  
+// a 
+// b 
+// _ 
+// thisIsVeryLongerVarialbeName   
+// a1b
+// a_b
+// a_bab1234
+// abcd
+// $a
+
+// Incorrect
+
+// 1ab- we cannot start wid Number
+// a-b - we cannot - in the variable 
+// a+b 
+
+
+
+//Take 2 inputs a and b show sum of both numbers
+ 
+var a=prompt("Enter first number");
+var b=prompt("Enter second number");
+
+n1=parseInt(a);
+n2=parseInt(b);
+
+// var sum =n1+n2;
+
+alert(n1+n2);
+
+
+
+
+
+
+
+
+
+
+

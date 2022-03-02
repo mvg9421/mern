85c2f510ef493ae255c425a053d4ac063dcc5c05
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-03-02 09:03:06 +0530

    Create assignment1.js

---
+++ 1stMar/assignment1.js
@@ -0,0 +1,18 @@
+var a=prompt("Enter 1st no");
+var b=prompt("Enter 2nd no");
+var c=prompt("Enter 3rd no");
+var n=parseInt(a);
+var m=parseInt(b);
+var o=parseInt(c);
+if(n<m&&n<o)
+{
+alert("n is smallest");
+}
+else if(m<n&&m<o)
+{
+alert("m is smallest");
+}
+else 
+{
+alert("o is smallest");
+}

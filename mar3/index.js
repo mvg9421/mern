2b251c2da8be6dd5cf97ddfcaaaf4ab8863cced0
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-03-03 11:16:01 +0530

    Create index.js

---
+++ mar3/index.js
@@ -0,0 +1,88 @@
+// var num=10;
+
+// if(num>0){
+//     console.log("Number is positive");
+// }else if(num==0){
+//     console.log("Number is 0");
+// }else{
+//     console.log("Number is negative")
+// }
+
+
+
+// var char="e";
+
+// if(char=='a' || char=='A'){
+//      console.log("it is vowel");
+// }else if(char=="e" || char=="E"){
+//     console.log("it is vowel");
+// }else if(char=="i" || char=="I"){
+//     console.log("it is vowel");
+// }else if(char=="0" || char=="O"){
+//     console.log("it is vowel");
+// }else if(char=="u" || char=="U"){
+//     console.log("it is vowel");
+// }else{
+//     console.log("it is a consonant");
+// }
+
+// var num=88;
+// var arr1=[1234, false,null, "Shub",num, ["Child elemet",44]];
+// console.log(arr1);
+// console.log(typeof arr1);
+// console.log(arr1[0]);
+
+// console.log(arr1[4]);
+
+// console.log(arr1[5][1]);  //Multi dimensional array
+
+
+// console.log(arr1[2]);
+// arr1[2]="New value";
+// console.log(arr1[2]);
+
+// console.log(arr1);
+
+// console.log(arr1[1]);
+// arr1[1]="New value 2";
+// console.log(arr1[1]);
+
+
+
+// Push
+
+// arr1.push("Inserted a last element");
+// console.log(arr1);
+
+
+var arr2=[
+    "ELement 1",
+    "ELement 2",
+    "ELement 3",
+    [
+        "Elememnt 4.1",
+        "Elememnt 4.2"
+    ],
+    "Element 5"
+];
+
+console.log(arr2);
+
+arr2[3].push("ELement 4.3");
+
+console.log(arr2[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

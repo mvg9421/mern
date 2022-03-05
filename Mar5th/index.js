1724bf2b509d21832e0461f06f574f3b7699a8c5
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-03-06 00:04:37 +0530

    Create index.js

---
+++ Mar5th/index.js
@@ -0,0 +1,102 @@
+// Print number from 1 to 10
+
+// var n=1;
+
+// while(n<=10){
+//     console.log(n);
+//     n=n+1;
+// }
+
+
+
+
+//Print the first 15 even numbers
+
+
+// var  n=2;
+
+// while (n<=30) {
+//     console.log(n);
+//     n=n+2;
+// }
+
+
+//Print the first n odd number and take input from user
+
+// var i=prompt("Enter a number");
+// i=parseInt(i);
+// var count=0;
+
+// while(count<i){
+//     console.log((2*count)+1);
+//     count=count+1;
+// }
+
+
+
+
+// var input=prompt("How many number u wNT TO INPY");
+
+// var arr1=[]
+
+
+// var i
+
+// for(i=0;i<input;i++){
+//     var values=prompt("Enter the numnber");
+//     arr1.push(values);
+// }
+
+// var maxnum=arr1[0];
+// for(i=0;i<input;i++){
+//     if(arr1[i]>maxnum){
+//         maxnum=arr1[i];
+//     }
+// }
+// alert("maximim number is " +maxnum);
+
+
+
+//Print number from from 1 to 10
+
+// var n=1;
+//      1    2    4
+// for(i=1;i<=10;i++){
+//    3 console.log(i);
+// }
+
+
+
+//Print upto 10, altleast print 1 
+// var n=10;
+// var count=1
+// do{
+//    console.log(count);
+//    count=count+1; 
+// }while(count<=n);
+
+
+
+// var s= "Hello Shubham";
+
+// //Split convert array of words
+// var a=s.split(" ");
+// console.log(a)
+
+// var s="1,2,3,4,5,6";
+// var a=s.split(",");
+// console.log(a);
+
+// var s="aghdxvshcbhcdbchdbcdcuhcbdhucbdhuc";
+
+// var a=s.split("c");
+// console.log(a);
+
+
+
+
+
+
+
+
+

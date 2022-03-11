0b1acca8df8707f46013771bc5ef0b1994559386
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-03-11 13:04:12 +0530

    Create index.js

---
+++ Mar11th/index.js
@@ -0,0 +1,84 @@
+
+//check whether character is a vowel or not
+
+// var character="i";
+
+
+// switch("I"){
+//     case 'a':
+//     case 'A':    
+//     console.log("Vowel entered a");
+//     break;
+//     case 'e':console.log("vowel is e");
+//     break;
+//     case 'i':
+//     case 'I':   
+//     console.log("vowel is i");
+//     break;
+//     case 'o':console.log("vowel is o");
+//     break;
+//     case 'u':console.log("vowel is u");
+//     break;
+//     default:console.log("it is a consonant")
+// }
+
+
+//Objects
+
+var Obj={
+    "firstname":"Shubham",
+    "last_name":"Mohan",
+    1:"Soemthing",
+    "a":12345,
+    "a":12345
+
+};
+
+// console.log(Obj);
+// console.log(Obj.firstname);
+
+
+// Obj.last_name="shubs";
+// console.log(Obj.last_name);
+
+// Obj.manthan="Sipna";
+// console.log(Obj);
+
+// delete Obj.manthan;
+
+// console.log(Obj)
+
+// console.log( Object.keys(Obj) );
+// console.log(Object.values(Obj));
+
+
+// Obj.experience={};
+
+// Obj.experience.years=7;
+// console.log(Obj);
+
+
+// Obj.students=[];
+
+// // console.log(Obj);
+
+// Obj.students.push("SHubham");
+// Obj.students.push("Manthan");
+// console.log(Obj);
+
+// console.log(Obj.students[1]);
+
+
+var keys= Object.keys(Obj);
+// console.log(keys);
+for(i=0;i<keys.length;i++){
+   console.log(keys[i]);
+}
+
+
+
+
+
+
+
+

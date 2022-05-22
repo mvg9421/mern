6a3bd7154f13794869eb9c1fe4aa24dde6c820b6
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-05-22 22:46:24 +0530

    Create palindrome.js

---
+++ May21st/palindrome.js
@@ -0,0 +1,114 @@
+// console.log("hello");
+
+
+// function Palindrome(e){
+// n=a.length;
+// for(var i=0;i<a.length/2;i++){
+//     if(e[i]!=e[n-1-i]){
+//         return "It is not a palindrome";
+//     }
+// }
+// return "It is a Palindrome";
+
+// }
+
+// var a="madam";
+// var result= Palindrome(a);
+// console.log(result);
+
+
+
+//WAP to find multiples of 2
+
+
+// function multiple(){
+  
+    
+
+
+// }
+
+
+// multiple();
+
+
+// var a="Teacher";
+
+// var Obj_name={
+//     'firstname':"Shubham",
+//     'Lastname':"Mohan",
+//     'Middlename':"Manoj",
+//     'ROle':a,
+//     'subjects':{
+//         'web':"React and JS",
+//         'backend':"Mongo"
+//     },
+    
+// }
+
+
+// console.log(Obj_name);
+// console.log(Obj_name.firstname);
+// console.log(Obj_name.Lastname);
+// console.log(Obj_name.ROle);
+// console.log(Obj_name.subjects.web);
+// console.log(Obj_name.subjects.backend);
+
+
+// var Obj_name={
+//     'firstname':"Shubham",
+//     'Lastname':"Mohan",
+//     'Middlename':"Manoj",
+//     'ROle':a,
+//     'subjects':{
+//         'web':"React and JS",
+//         'backend':"Mongo"
+//     },
+    
+// }
+
+
+
+//If u want to access variable outside the object and thaT VARIABLE IS and obj then use x[varname/objname]
+
+
+
+
+// var x={};
+
+// var y={'names':"Shubham"};
+
+// x[y]={'names':"Manthan"}
+
+// console.log(x.y);
+// console.log(x[y]);
+
+// console.log(x);
+
+
+
+// x={
+//    "[object Object]" : {
+//        'names':"Manthan"
+//    }
+
+
+// }
+
+
+
+
+
+// Assignment:prompt = take  1 number
+// parseInt, i3 and 5 both then printf  a number is->
+//print(document.write)\
+// input=10,
+//1,2,3,4,5,6,7,8,9,10,,
+//1,2,fizz,4,buzz,fizz,7,8,fizz,buzz
+//if number is divsible by 3 ptint Fizz
+//if number is divisible by 5 print Buzz
+//if number is divisible by FizzBuzzf
+
+
+
+

167124f49f3caf5107d71eab2a58edc6e20bc00f
mvg9421 <100397599+mvg9421@users.noreply.github.com>
2022-05-22 22:47:45 +0530

    Create index.js

---
+++ May16th/index.js
@@ -0,0 +1,121 @@
+// alert("1234");
+
+// document.write("Document write");
+// console.log("It will write in console");
+
+
+//WAP to find number is even or odd
+// function check_oddeven(num1){
+//     if(num1 % 2 == 0){
+//      document.write("Even number")
+
+//     }else{
+//         document.write("Odd number");
+//     }
+// square(input2);
+// }
+
+// check_oddeven(5);
+
+
+//WAP to find square of number
+
+// function square(n){
+//   document.write(n*n);
+// }
+// square(4);
+
+
+
+// function square(n){
+//     var sqr=n*n;
+//     document.write(sqr);
+// }
+  
+
+
+
+// var input= prompt("ENter the number");
+// input= parseInt(input);
+// square(input);
+
+
+
+// function square(n){
+//     var sqr=n*n;
+//     return sqr;
+// }
+  
+
+// var input= prompt("ENter the number");
+// input= parseInt(input);
+// var outputvar=  square(input);
+// document.write(outputvar);
+
+
+
+//wap to add 2 numbers
+
+// function add(num1,num2){
+//     var addition=num1+num2;
+//     return addition;
+// }
+
+
+// var a=prompt("enter first number");
+// a=parseInt(a);
+
+// var b=prompt("enter 2nd number");
+// b=parseInt(b);
+
+// var s= add(a,b);
+// document.write(s);
+
+
+// function add(num1,num2){
+//     var addition=num1+num2;
+//     document.write(addition);
+// }
+
+
+// var a=prompt("enter first number");
+// a=parseInt(a);
+
+// var b=prompt("enter 2nd number");
+// b=parseInt(b);
+
+//  add(a,b);
+
+
+
+
+//print  numbers upto n
+
+// function printnum(n){
+//    for(var i=1;i<=n;i++){
+// document.write(i);
+//    }
+// }
+
+
+// printnum(5);
+
+
+// shubham
+
+
+
+function check(nam){
+    var len=nam.length;  
+  for(var i=0;i<len;i++){  
+      if(nam[i]=="u"){           
+        document.write(i); 
+      }
+  }
+}
+
+check("Shubham");
+
+
+
+
